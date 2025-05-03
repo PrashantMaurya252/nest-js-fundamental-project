68cd04553ac9dd406bf194335b7f7163106d7330
PrashantMaurya252 <mauryaprashant252@gmail.com>
2025-05-03 20:57:06 +0530

    applying middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,24 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule, RequestMethod } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SongsModule } from './songs/songs.module';
+import { LoggerMiddleware } from './common/middleware/logger/logger.middleware';
 
 @Module({
   imports: [SongsModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    // options 1 : for songs routes only
+      consumer.apply(LoggerMiddleware).forRoutes('songs')
+
+      // options2: if we want on certain type of requests
+
+      // consumer.apply(LoggerMiddleware).forRoutes({path:'songs',method:RequestMethod.POST})
+
+      // option3 
+      // consumer.apply(LoggerMiddleware).forRoutes(SongsController)
+  }
+}
